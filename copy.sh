730a60d7f1461f0d44f59d8b49ce0b4960393e68
Joep Kockelkorn <joepkockelkorn@gmail.com>
2019-09-30 20:54:36 +0200

    feat: 🎸 implement borderfy

--- copy.sh
+++ copy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+mkdir -p dist
 cp manifest.json ./dist/manifest.json
-cp print_16x16.png ./dist/print_16x16.png
+
+iconsizes=(16 32 48 64 128)
+for size in "${iconsizes[@]}"; do
+    cp "./icons/${size}x${size}.png" "./dist/${size}x${size}.png"
+done
+    # cp print_16x16.png ./dist/print_16x16.png
